fb97ff4c097b9a2d1b5f97ed8d65fb171ec00e1a
EllaMarcum <ecaroline_29@icloud.com>
2022-08-09 10:53:50 -0600

    commit

--- client/app/env.js
+++ client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-05dywz26.us.auth0.com'
+export const audience = 'https://codeclassroom.com'
+export const clientId = 'On5PbKGLgflR1Qzp4VZn9Ka06v7eTh6r'
